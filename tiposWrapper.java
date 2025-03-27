1458a7faa8793692411844845071ce945ede56a3
pedrocarrara12 <pepe.carrara@gmail.com>
2025-03-27 18:54:32 -0400

    Exercicio Wrappers java

---
+++ tiposWrapper.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class tiposWrapper {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Digite algum numero :  ");
+        int x= sc.nextInt();
+        Integer xWrapper=x;
+        System.out.println(xWrapper);
+
+
+    }
+}
